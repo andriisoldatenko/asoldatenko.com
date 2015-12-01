979891f7642e22db11fcca1592665682e33369bb
Andrii Soldatenko <andrii.soldatenko@datarobot.com>
2015-12-01 21:04:43 +0200

    Initial commit

---
+++ content/blog/selenium-camp-2015.rst
@@ -0,0 +1,47 @@
+High performance functional tests based on Selenium - SeleniumCamp 2015
+#######################################################################
+
+:category: talks
+:date: 2015-03-22
+
+
+Abstract
+--------
+
+    "Time is at once the most valuable and the most perishable of all our possessions."
+    -- John Randolph
+
+Correspondingly we must know how to improve a quality of the project in the limitted timeframes. The goal of my presentation is improving an execution time of automated functional tests based on Selenium Webdriver, by using, for instance, parallel execution, scaling by distributing tests on several machines, creating strategy for generation of big sets of test data for typical project. I am pleased to share with you my acquired experience in this field.
+
+
+Video
+`````
+
+You can find my video from `Selenium Camp`_ 2015.
+
+.. _Selenium Camp: http://seleniumcamp.com/materials/high-performance-selenium-tests/
+
+
+.. youtube:: FyhCsxH-Xfw
+    :align: center
+    :width: 750
+    :height: 500
+
+
+About Selenim Camp Conference
+`````````````````````````````
+
+Selenium Camp is automated testing conference in Europe, which is focused on usage of Selenium Webdriver.
+
+.. youtube:: lg-hQiDXUoY
+    :align: center
+    :width: 750
+    :height: 500
+
+Slides
+``````
+
+My slides from `Selenium Camp`_ 2015.
+
+{slideshare id=45278504&doc=selenium-camp2015-andriisoldatenko}
+
---
+++ pelicanconf.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Andrii Soldatenko'
+SITENAME = u'Python Geek'
+SITESUBTITLE = u'Blog about interesting things'
+#SITEURL = 'https://asoldatenko.com/'
+SITEDESCRIPTION = 'Andrii Soldatenko blog about Python'
+
+ARTICLE_PATHS = ['blog']
+
+
+PATH = 'content'
+
+#ARTICLE_SAVE_AS = '{date:%Y}/{slug}.html'
+#ARTICLE_URL = '{date:%Y}/{slug}.html'
+GITHUB_URL = 'http://github.com/andriisoldatenko/'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('@a_soldatenko', 'http://twitter.com/a_soldatenko'),
+                  ('andriisoldatenko', 'http://github.com/andriisoldatenko'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+THEME = 'pelicanyan'
+GA_ACCOUNT = 'UA-12344321-1'
+TWITTER_ACCOUNT = 'getpelican'
+DIRECT_TEMPLATES = ('index', 'categories', 'authors', 'archives', 'sitemap', 'robots', 'humans')
+ROBOTS_SAVE_AS = 'robots.txt'
+HUMANS_SAVE_AS = 'humans.txt'
+SITEMAP_SAVE_AS = 'sitemap.xml'
+DEFAULT_LANG = 'en'
+DATE_FORMATS = { 'en': '%B %d, %Y', }
+STATIC_PATHS = ['static', 'images', 'favicon.ico']
+TYPOGRIFY=True
+
+PLUGIN_PATHS = ['plugins']
+
+PLUGINS = [
+    'slideshare',
+    'pelican_youtube'
+]
